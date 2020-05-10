b0f78e6ad2fc9bfd453155734a3be87af0cc99eb
fachrulch <fachrul.fch@gmail.com>
2020-05-10 07:36:46 +0700

    [python-selenium] add new step in form

--- python-selenium/bagian-2/pages/form.py
+++ python-selenium/bagian-2/pages/form.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import time
+from selenium.webdriver.support.select import Select
 
 class Form():
     URL = 'https://formy-project.herokuapp.com/form'
@@ -7,6 +9,11 @@ class Form():
     FIRST_NAME = (By.ID, 'first-name')
     LAST_NAME = (By.ID, 'last-name')
     SUBMIT = (By.LINK_TEXT, 'Submit')
+    JOB_TITLE = (By.ID, 'job-title')
+    EDUCATION = (By.ID, 'radio-button-{}')
+    GENDER = (By.ID, 'checkbox-{}')
+    EXPERIENCE = (By.ID, 'select-menu')
+
 
     def __init__(self, browser: webdriver.Remote):
         self.browser = browser
@@ -15,8 +22,31 @@ class Form():
         self.browser.get(self.URL)
 
     def input_name(self, first_name, last_name):
-        self.browser.find_elements(*self.FIRST_NAME).send_keys(first_name)
-        self.browser.find_elements(*self.LAST_NAME).send_keys(last_name)
+        self.browser.find_element(*self.FIRST_NAME).send_keys(first_name)
+        self.browser.find_element(*self.LAST_NAME).send_keys(last_name)
 
     def submit_form(self):
         self.browser.find_element(*self.SUBMIT).click()
+        time.sleep(3)
+
+    def input_job_title(self, job_title):
+        self.browser.find_element(*self.JOB_TITLE).send_keys(job_title)
+
+    def choose_education(self, education_level: str):
+        level = {'high school': 1, 'college': 2, 'grad school': 3}
+        edu_level = level.get(education_level.lower(), 1)
+        locator = list(self.EDUCATION)
+        locator[1] = locator[1].format(edu_level)
+        self.browser.find_element(*locator).click()
+
+    def choose_gender(self, user_gender: str):
+        gender_option = {'male': 1, 'female': 2}
+        gender = gender_option.get(user_gender.lower(), 3)
+        _by, _val = self.GENDER
+        _val = _val.format(gender)
+        self.browser.find_element(_by, _val).click()
+
+    def choose_experience(self, user_experience: str):
+        print('user experience: ', user_experience)
+        select = Select(self.browser.find_element(*self.EXPERIENCE))
+        select.select_by_visible_text(user_experience)
--- python-selenium/bagian-2/tests/test_form.py
+++ python-selenium/bagian-2/tests/test_form.py
@@ -1,9 +1,20 @@
 from selenium import webdriver
 from pages.form import Form
+import time
+from pages.thanks import Thanks
 
 
 class TestForm():
     def test_submit_name(self, browser: webdriver.Remote):
         form_page = Form(browser)
         form_page.load()
-        
+        form_page.input_name('Fachrul', 'Choliluddin')
+        form_page.choose_education('Grad School')
+        form_page.choose_gender('Female')
+        form_page.choose_experience('2-4')
+        time.sleep(3)
+        # form_page.submit_form()
+
+        # thanks_page = Thanks(browser)
+        # assert thanks_page.title == 'Thanks for submitting your form'
+        # assert thanks_page.message == 'The form was successfully submitted!'
